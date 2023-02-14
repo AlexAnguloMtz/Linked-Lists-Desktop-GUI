56c8e433866b1342f263faf02c2346e3611d46fb
alex <raulalexo100@gmail.com>
2023-02-14 11:52:04 -0700

    Refactored Pair from class to a record

--- src/main/java/com/demo/list/util/Pair.java
+++ src/main/java/com/demo/list/util/Pair.java
@@ -1,25 +1,7 @@
 package com.demo.list.util;
 
-public class Pair<T, U> {
-
-    private final T left;
-    private final U right;
-
-    private Pair(T t, U u) {
-        this.left = t;
-        this.right = u;
+public record Pair<T, U> (T left, U right) {
+    public static <T, U > Pair<T, U> of(T left, U right) {
+        return new Pair<>(left, right);
     }
-
-    public static <T, U > Pair of(T t, U u) {
-        return new Pair<>(t, u);
-    }
-
-    public T left() {
-        return left;
-    }
-
-    public U right() {
-        return right;
-    }
-
 }
